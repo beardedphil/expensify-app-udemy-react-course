cac08ea48260470ed42d9a24741d1408453ee434
Phil King <phil@beardedphil.com>
2020-03-03 15:54:14 -0600

    End of lecture 168 - Finished PublicRoute and PrivateRoute components to keep users where they're allowed to be.  Updated expense actions and associated tests to conform to new database structure.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux'
 import { startLogout } from '../actions/auth';
 
@@ -7,7 +7,7 @@ import { startLogout } from '../actions/auth';
 export const Header = ({ startLogout }) => (
     <header>
         <h1>Expensify</h1>
-        <NavLink to="/" activeClassName="is-active" exact={ true }>Dashboard</NavLink>
+        <NavLink to="/dashboard" activeClassName="is-active">Dashboard</NavLink>
         <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
         <button
             onClick={ startLogout }
